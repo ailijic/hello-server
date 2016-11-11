44ed8fc7d4e36e7a99705c729e71790bdd12571c
Andrew Ilijic <ailijic@gmail.com>
2016-11-11 08:08:42 -0500

    The server uses the first 2 arguments of the path to print the users name.

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ app.get('/', function(request, response) {
   response.send("Hello World");
 });
 
+app.get('/:firstname/:lastname', function(request, response) {
+  let first = request.params.firstname;
+  let last = request.params.lastname;
+  response.send(["Hello", first, last].join(" "));
+});
+
 app.listen(process.env.PORT || 8080, process.env.IP);
