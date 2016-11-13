f0fedd219d21158946b6008fe2dd6add0d1910c0
Andrew Ilijic <ailijic@gmail.com>
2016-11-13 08:39:57 -0500

    Done

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 "use strict";
 
 let express = require('express');
-
 let app = express();
 
-app.get('/', function(request, response) {
-  response.send("Hello World");
-});
+app.get('/', respond);
+app.get('/:arg1', respond);
+app.get('/:arg1/:arg2', respond);
 
-app.get('/:firstname/:lastname', function(request, response) {
-  let first = request.params.firstname;
-  let last = request.params.lastname;
-  response.send(["Hello", first, last].join(" "));
-});
-
+function respond (req, res) {
+  const arg1 = req.params.arg1;  
+  const arg2 = req.params.arg2;  
+  switch (arg1) {
+    case "headers":
+			sendHeaders(arg2);
+    case "json":
+      res.json({
+        name: 'Kim Gordon',
+        instrument: 'Bass'
+      });
+      break;
+    case "version":
+      res.json(req.httpVersion);
+      break;
+    default:
+      sendText(arg1, arg2);
+  }
+  function sendHeaders (arg) {
+    if (arg === undefined) {
+      res.json(req.headers);
+    } else {
+      res.json(req.headers[arg]);
+    }
+  }
+  function sendText (arg1, arg2) {
+    if ((arg1 === undefined) && (arg2 === undefined)) {
+      res.send("Hello World");
+    } else if (arg2 !== undefined) {
+      res.send(["Hello", arg1, arg2].join(" "));
+    } else {
+      console.log("Something Went Wrong");
+    }
+  }
+}
 app.listen(process.env.PORT || 8080, process.env.IP);
